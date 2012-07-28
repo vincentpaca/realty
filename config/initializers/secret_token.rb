33ba659e245f5a23552e5de6bd711b765a9f3f03
Vincent Paca <vincent.paca@gmail.com>
2012-07-28 17:07:01 +0800

    Initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realty::Application.config.secret_token = 'f472c07b0b0c015c578b4ec94c148a7bd7636249f96dca994a404f87f7324c61d8899c3601d432e9624f6d393a0ec00c92410ece9839b62eb851043843b8d051'
