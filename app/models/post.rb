6b188dde70ba6751702eefe1c4d261fa3d57f0ed
Vincent Paca <vincent.paca@gmail.com>
2012-07-29 21:04:54 +0800

    Added category as attr_accessible

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :ad_type, :description, :location, :price, :property_type
+  attr_accessible :ad_type, :category, :description, :location, :price, :property_type
   
   geocoded_by :location
   after_validation :geocode
