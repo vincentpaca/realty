5eaa4da9d03c42352292aa917fd8d859e9a4795a
Vincent Paca <vincent.paca@gmail.com>
2012-07-29 20:18:56 +0800

    Setup the associations between posts, categories and ad_types

--- app/models/ad_type.rb
+++ app/models/ad_type.rb
@@ -1,3 +1,5 @@
 class AdType < ActiveRecord::Base
   attr_accessible :name
+  
+  has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
-  attr_accessible :advertised_as, :description, :location, :price, :property_type
+  attr_accessible :ad_type, :description, :location, :price, :property_type
   
   geocoded_by :location
   after_validation :geocode
+  
+  belongs_to :ad_type
+  belongs_to :category
 end
