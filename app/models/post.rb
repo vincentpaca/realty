0cb4f36e769cfa8d2d3154f4030f40238c3e699c
Vincent Paca <vincent.paca@gmail.com>
2012-08-04 20:25:10 +0800

    Added validators

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ActiveRecord::Base
-  attr_accessible :ad_type, :category, :description, :location, :price
+  attr_accessible :ad_type, :category, :description, :location, :price, :title
   
   geocoded_by :location
   after_validation :geocode
   
   belongs_to :ad_type
   belongs_to :category
+
+  validates_presence_of :description, :title, :location, :price
+  validates_length_of :description, :minimum => 20
 end
